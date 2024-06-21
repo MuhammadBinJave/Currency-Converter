83ac7a2837f98a0e679eccb91237326a50f31069
KhulaJaved <khulajaved1995@gmail.com>
2024-06-21 17:40:00 +0500

    currency converter

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency:any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+
+let userAnswer: any = await inquirer.prompt(
+    [
+        {
+            name:'from',
+            message:'Enter from currency',
+            type:'list',
+            choices:['USD', 'EUR', 'GBP', 'INR', 'PKR']
+        },
+
+        {
+            name:'to',
+            message:'Enter To Currency',
+            type:'list',
+            choices:['USD', 'EUR', 'GBP', 'INR', 'PKR']
+        },
+
+        {
+            name:'amount',
+            message:'Enter your Amount',
+            type:'number'
+        }
+    ]
+)
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount);
